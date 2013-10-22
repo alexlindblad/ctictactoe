40d87ccc1f236f9c43f97a1b0b43d284eca4f057
Alex J Lindblad <alex.lindblad@gmail.com>
2013-10-22 01:44:10 -0700

    initial functioning game

--- CTicTacToe/CTicTacToe/CGameBoard.c
+++ CTicTacToe/CTicTacToe/CGameBoard.c
@@ -8,24 +8,21 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
+#include <assert.h>
 #include "CGameBoard.h"
 
 static const int kCBoardWidth = 3;
 static const int kCBoardHeight = 3;
 static const int kCNumBoardCells = kCBoardHeight*kCBoardWidth;
-typedef enum CellEntry {
-    kCCellNoEntry = -1,
-    kCCellEntryX,
-    kCCellEntryO
-} CellEntry;
 
 struct CGameCell{
-    CellEntry _entry;
+    CellEntry entry;
 };
 typedef struct CGameCell Cell;
 
 struct CGameBoard {
-    Cell _cells[kCNumBoardCells];
+    Cell cells[kCNumBoardCells];
 };
 
 /*!
@@ -33,6 +30,10 @@ struct CGameBoard {
 */
 void CGameBoard_Init(GameBoard* board);
 void CGameBoard_DrawCell(Cell cell);
+bool CGameBoard_CellAvailable(GameBoard* board, RowIndicator row, ColIndicator col);
+int CGameBoard_CellIndex(RowIndicator row, ColIndicator col);
+bool CGameBoard_CellsHaveSameEntries(GameBoard* board, int i, int j, int k);
+
 /*!
 * public methods
 */
@@ -43,37 +44,38 @@ GameBoard* CGameBoard_NewBoard(void) {
 }
 
 void CGameBoard_Draw(GameBoard* board) {
-
+    assert(board != NULL);
+    
     printf("    1   2   3  \n");
     printf("      |   |    \n");
     printf("A   ");
-    CGameBoard_DrawCell(board->_cells[0]);
+    CGameBoard_DrawCell(board->cells[0]);
     printf(" | ");
-    CGameBoard_DrawCell(board->_cells[1]);
+    CGameBoard_DrawCell(board->cells[1]);
     printf(" | ");
-    CGameBoard_DrawCell(board->_cells[2]);
+    CGameBoard_DrawCell(board->cells[2]);
     printf("  \n");
     
     printf("      |   |    \n");
     printf("   ---+---+--- \n");
     printf("      |   |    \n");
     printf("B   ");
-    CGameBoard_DrawCell(board->_cells[3]);
+    CGameBoard_DrawCell(board->cells[3]);
     printf(" | ");
-    CGameBoard_DrawCell(board->_cells[4]);
+    CGameBoard_DrawCell(board->cells[4]);
     printf(" | ");
-    CGameBoard_DrawCell(board->_cells[5]);
+    CGameBoard_DrawCell(board->cells[5]);
     printf("  \n");
     
     printf("      |   |    \n");
     printf("   ---+---+--- \n");
     printf("      |   |    \n");
     printf("C   ");
-    CGameBoard_DrawCell(board->_cells[6]);
+    CGameBoard_DrawCell(board->cells[6]);
     printf(" | ");
-    CGameBoard_DrawCell(board->_cells[7]);
+    CGameBoard_DrawCell(board->cells[7]);
     printf(" | ");
-    CGameBoard_DrawCell(board->_cells[8]);
+    CGameBoard_DrawCell(board->cells[8]);
     printf("  \n");
     printf("      |   |    \n\n");
 }
@@ -83,17 +85,47 @@ void CGameBoard_Clean(GameBoard* board) {
         free(board);
 }
 
+CellSelectError CGameBoard_SelectCell(GameBoard* board,
+                                      RowIndicator row,
+                                      ColIndicator col,
+                                      CellEntry entry) {
+    assert(board != NULL);
+    
+    if (!CGameBoard_CellAvailable(board, row, col)) {
+        return kCCellSelectUnavailable;
+    }
+    
+    board->cells[CGameBoard_CellIndex(row, col)].entry = entry;
+    return kCCellSelectNoError;
+}
+
+bool CGameBoard_HasWinner(GameBoard* board) {
+    // check rows
+    return (CGameBoard_CellsHaveSameEntries(board, 0, 1, 2) ||
+            CGameBoard_CellsHaveSameEntries(board, 3, 4, 5) ||
+            CGameBoard_CellsHaveSameEntries(board, 6, 7, 8) ||
+    // check columns
+            CGameBoard_CellsHaveSameEntries(board, 0, 3, 6) ||
+            CGameBoard_CellsHaveSameEntries(board, 1, 4, 7) ||
+            CGameBoard_CellsHaveSameEntries(board, 2, 5, 8) ||
+    // check diagonals
+            CGameBoard_CellsHaveSameEntries(board, 0, 4, 8) ||
+            CGameBoard_CellsHaveSameEntries(board, 2, 4, 6));
+}
+
 /*!
 * Internal method implementation
 */
 void CGameBoard_Init(GameBoard* board) {
+    assert(board != NULL);
+    
     for (int i = 0; i < kCNumBoardCells; i++) {
-        board->_cells[i]._entry = kCCellNoEntry;
+        board->cells[i].entry = kCCellNoEntry;
     }
 }
 
 void CGameBoard_DrawCell(Cell cell) {
-    switch (cell._entry) {
+    switch (cell.entry) {
         case kCCellEntryX:
             printf("X");
             break;
@@ -106,4 +138,39 @@ void CGameBoard_DrawCell(Cell cell) {
         break;
 
     }
+}
+
+int CGameBoard_CellIndex(RowIndicator row, ColIndicator col) {
+    int index = 0;
+    if (row == kCRowB) {
+        index += kCBoardWidth;
+    } else if (row == kCRowC) {
+        index += 2*kCBoardWidth;
+    }
+    
+    if (col == kCCol2) {
+        index += 1;
+    } else if (col == kCCol3) {
+        index += 2;
+    }
+    return index;
+}
+
+bool CGameBoard_CellAvailable(GameBoard* board, RowIndicator row, ColIndicator col) {
+    assert(board != NULL);
+    
+    int index = CGameBoard_CellIndex(row, col);
+    return (board->cells[index].entry == kCCellNoEntry);
+}
+
+bool CGameBoard_CellsHaveSameEntries(GameBoard* board, int i, int j, int k) {
+    assert(i >= 0 && i < kCNumBoardCells);
+    assert(j >= 0 && j < kCNumBoardCells);
+    assert(k >= 0 && k < kCNumBoardCells);
+    assert(board != NULL);
+
+    return (board->cells[i].entry != kCCellNoEntry &&
+            board->cells[i].entry == board->cells[j].entry &&
+            board->cells[j].entry == board->cells[k].entry);
+
 }
--- CTicTacToe/CTicTacToe/CGameBoard.h
+++ CTicTacToe/CTicTacToe/CGameBoard.h
@@ -9,6 +9,33 @@
 #ifndef CTicTacToe_CGameBoard_h
 #define CTicTacToe_CGameBoard_h
 
+#include <stdbool.h>
+
+typedef enum CellEntry {
+    kCCellNoEntry = -1,
+    kCCellEntryX,
+    kCCellEntryO
+} CellEntry;
+
+typedef enum RowIndicator {
+    kCRowInvalid,
+    kCRowA,
+    kCRowB,
+    kCRowC
+} RowIndicator;
+
+typedef enum ColIndicator {
+    kCColInvalid,
+    kCCol1,
+    kCCol2,
+    kCCol3
+} ColIndicator;
+
+typedef enum CellSelectError {
+    kCCellSelectNoError = 0,
+    kCCellSelectUnavailable
+} CellSelectError;
+
 struct CGameBoard;
 typedef struct CGameBoard GameBoard;
 
@@ -17,5 +44,12 @@ GameBoard* CGameBoard_NewBoard(void);
 void CGameBoard_Draw(GameBoard* board);
 
 void CGameBoard_Clean(GameBoard* board);
+
+CellSelectError CGameBoard_SelectCell(GameBoard* board,
+                                      RowIndicator row,
+                                      ColIndicator col,
+                                      CellEntry entry);
+
+bool CGameBoard_HasWinner(GameBoard* board);
 
 #endif
