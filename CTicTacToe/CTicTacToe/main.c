ebf7f6c8d248f10a65b4aa8d2f8990efd9a01d6f
Alex J Lindblad <alex.lindblad@gmail.com>
2013-10-21 23:48:07 -0700

    initial project

---
+++ CTicTacToe/CTicTacToe/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  CTicTacToe
+//
+//  Created by Alex Lindblad on 10/21/13.
+//  Copyright (c) 2013 Alex Lindblad. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
